bb7f136794e94921b79b21de6d43c124eb651f00
waynechoidev <wonjun92@gmail.com>
2024-04-12 15:25:16 +1200

    feat: add skybox

---
+++ src/engine/geometry/skybox.ts
@@ -0,0 +1,99 @@
+import { vec2, vec3, mat4 } from "@external/glmatrix/index";
+import Mesh from "./mesh";
+
+export default class Skybox extends Mesh {
+  constructor(device: GPUDevice) {
+    super(device, "skybox");
+
+    this._vertices.push({
+      position: vec3.fromValues(-1, 1, -1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+    this._vertices.push({
+      position: vec3.fromValues(-1, -1, -1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+    this._vertices.push({
+      position: vec3.fromValues(1, 1, -1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+    this._vertices.push({
+      position: vec3.fromValues(1, -1, -1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+
+    this._vertices.push({
+      position: vec3.fromValues(-1, 1, 1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+    this._vertices.push({
+      position: vec3.fromValues(1, 1, 1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+    this._vertices.push({
+      position: vec3.fromValues(-1, -1, 1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+    this._vertices.push({
+      position: vec3.fromValues(1, -1, 1),
+      normal: vec3.fromValues(0, 0, 0),
+      tangent: vec3.fromValues(0, 0, 0),
+      texCoord: vec2.fromValues(0, 0),
+    });
+
+    this._indices.push(
+      0,
+      1,
+      2,
+      2,
+      1,
+      3, // Front
+      2,
+      3,
+      5,
+      5,
+      3,
+      7, // Right
+      5,
+      7,
+      4,
+      4,
+      7,
+      6, // Back
+      4,
+      6,
+      0,
+      0,
+      6,
+      1, // Left
+      4,
+      0,
+      5,
+      5,
+      0,
+      2, // Top
+      1,
+      6,
+      3,
+      3,
+      6,
+      7 // Bottom
+    );
+
+    this.initialize();
+  }
+}
